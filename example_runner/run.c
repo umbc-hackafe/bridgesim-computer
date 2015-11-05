9713efa89b2ec9c9a21cc69ba4d8d5a95b44c42c
Zachary Stewart <zachary@zstewart.com>
2015-11-05 00:16:27 -0500

    added a run example

---
+++ example_runner/run.c
@@ -0,0 +1,38 @@
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "motherboard/motherboard.h"
+#include "ram/ram.h"
+
+void* mb = 0;
+
+void handle_sigint(int);
+
+int main() {
+    // Create a ram device with 256KiB
+    struct Device* ram_device = make_ram_device(0x40000);
+    printf("Created a ram device at %p.\n", ram_device);
+
+    // Create a motherboard with space for four devices
+    mb = bscomp_motherboard_new(4);
+    printf("Created a motherboard at %p.\n", mb);
+
+    // Add the ram device to the motherboard
+    bscomp_motherboard_add_device(mb, ram_device);
+    printf("Attached the ram device to the motherboard.\n");
+
+    printf("Booting!\n");
+    bscomp_motherboard_boot(mb);
+
+    // Cleanup the motherboard
+    bscomp_motherboard_destroy(mb);
+    mb = 0;
+    printf("Destroyed motherboard.\n");
+
+    // Cleanup the ram device
+    delete_ram_device(ram_device);
+    printf("Destroyed ram device.\n");
+
+    return 0;
+}
